efe5eaee69c15a4df539835065a18800c9a85603
mayteet <luzmayteet@gmail.com>
2022-12-09 06:18:56 -0500

    final commit

--- app/main.py
+++ app/main.py
@@ -14,17 +14,27 @@ async def raiz():
 
 @app.get("/get_max_duration/{anio},{plataforma},{um}")
 async def get_max_duration(anio:int, plataforma:str, um:str):
-    respuesta=df_final["type"]
-    return respuesta
+    df_consul_1= df_final[["plataform","release_year","um","duration_3"]]
+    df_consul_1=df_consul_1[(df_consul_1["um"]==um)&(df_consul_1["release_year"]==anio)&(df_consul_1["plataform"]==plataforma)].groupby("release_year").max()
+    return df_consul_1
 
 @app.get("/get_count_platform/{plataforma}")
-async def validar_capicua(plataforma:str):
-    plataforma=" No es capicúa"
+async def get_count_platform(plataforma:str):
+    df_consul_2= df_final[["plataform","title"]]
+    df_consul_2=df_consul_2.groupby([plataforma]).count()
+    return df_consul_2
     
 @app.get("/get_listedin/{genero}")
-async def validar_capicua(genero:str):
-    respuesta=" No es capicúa"
+async def get_listedin(genero:str):
+    df_consul_3= df_final[["plataform","listed_in"]]
+    df_consul_3=df_consul_3[df_consul_3["listed_in"].str.contains(genero, regex=False)].groupby("plataform").count()
+    return df_consul_3
     
-@app.get("/get_actor/{plataforma}{año}")
-async def validar_capicua(plataforma:str, año:int):
-    respuesta=" No es capicúa"
+@app.get("/get_actor/{actor}{anio}")
+async def get_actor(actor:str, anio:int):
+    df_consul_4=df_final[["plataform","cast","year_added"]]
+    df_consul_4= df_consul_4.dropna(axis=0, how="all", subset=["cast"])
+    df_consul_4=df_consul_4[df_consul_4["cast"].str.contains(actor, regex=False)]
+    df_consul_4= df_consul_4[df_consul_4["year_added"]==anio]
+    df_consul_4=df_consul_4["cast"].count()
+    return df_consul_4
--- python_code/consultas.py
+++ python_code/consultas.py
@@ -2,13 +2,13 @@
 import requests
 import pandas as pd
 
+
 #Read data from github
 
 #Archivos json
 url="https://raw.githubusercontent.com/mayteet/ProyectoIndividual/main/datasets/netflix_titles.json"
 f=requests.get(url)
 data=f.json()
-
 # Convertir el archivo de json en un DataFrame
 netflix=pd.DataFrame(data)
 
@@ -51,6 +51,8 @@ data_all
 #Limpieza de datos
 data_all.rename(columns={'duration':"duration_2"}, inplace=True)
 
+
+
 #Juntamos todo en la tabla principal
 df_final= pd.merge(df,data_all, on=["show_id","plataform"],how="left")
 
@@ -72,4 +74,33 @@ df_final["um"]=df_final["um"].str.replace("Seasons","Season")
 
 #Quitamos los duplicados de la tabla
 df_final=df_final.drop_duplicates(["title","type","plataform"])
+df_final
 
+
+#CONSULTA 1
+
+df_consul_1= df_final[["plataform","release_year","um","duration_3"]]
+df_consul_1=df_consul_1[df_consul_1["um"]=="min"].max()
+df_consul_1
+
+
+#CONSULTA 2
+df_consul_2= df_final[["plataform","title"]]
+df_consul_2=df_consul_2.groupby(["plataform"]).count()
+df_consul_2
+
+#CONSULTA 3
+df_consul_3= df_final[["plataform","listed_in"]]
+df_consul_3=df_consul_3[df_consul_3["listed_in"].str.contains("TV Shows", regex=False)].groupby("plataform").count()
+list= set(df_consul_3["listed_in"].tolist())
+df_consul_3
+
+
+#CONSULTA 4
+#CONSULTA 4
+df_consul_4=df_final[["plataform","cast","year_added"]]
+df_consul_4= df_consul_4.dropna(axis=0, how="all", subset=["cast"])
+df_consul_4=df_consul_4[df_consul_4["cast"].str.contains("Nell Carter", regex=False)]
+df_consul_4= df_consul_4[df_consul_4["year_added"]==2019]
+df_consul_4=df_consul_4["cast"].count()
+list= set(df_consul_4["cast"].tolist())
